c8cdeceb8294a0fb382bc1a88df3510dd5ce3729
fred <fred@dilger.ca>
2023-10-30 18:02:34 -0600

    Removed Skill Screen

--- src/main/java/net/dilger/sky_forge_mod/client/ClientEvents.java
+++ src/main/java/net/dilger/sky_forge_mod/client/ClientEvents.java
@@ -1,7 +1,6 @@
 package net.dilger.sky_forge_mod.client;
 
 import net.dilger.sky_forge_mod.SkyForgeMod;
-import net.dilger.sky_forge_mod.gui.screen.skill_tree.SkillScreen;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.Component;
 import net.minecraftforge.api.distmarker.Dist;
@@ -9,7 +8,6 @@ import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 
 public class ClientEvents {
@@ -20,9 +18,6 @@ public class ClientEvents {
         public static void onKeyInput(InputEvent.Key event) {
             if(KeyBinding.keySkills.consumeClick()) {
                 Minecraft.getInstance().player.sendSystemMessage(Component.literal("Pressed Screen Key!"));
-
-                DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> Minecraft.getInstance().setScreen(new SkillScreen()));
-
             }
         }
 
--- src/main/java/net/dilger/sky_forge_mod/skills/Perk.java
+++
@@ -1,4 +0,0 @@
-package net.dilger.sky_forge_mod.skills;
-
-public class Perk {
-}

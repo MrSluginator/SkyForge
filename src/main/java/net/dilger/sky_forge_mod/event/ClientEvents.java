c8a7ae1277fc0a992317bf85a49f7c816fb2ad00
fred <fred@dilger.ca>
2023-10-31 00:20:48 -0600

    Added close button P for ExampleScreen

--- src/main/java/net/dilger/sky_forge_mod/event/ClientEvents.java
+++ src/main/java/net/dilger/sky_forge_mod/event/ClientEvents.java
@@ -1,12 +1,12 @@
-package net.dilger.sky_forge_mod.client;
+package net.dilger.sky_forge_mod.event;
 
 import net.dilger.sky_forge_mod.SkyForgeMod;
+import net.dilger.sky_forge_mod.client.ClientHooks;
 import net.dilger.sky_forge_mod.util.KeyBinding;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.Component;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.InputEvent;
-import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -14,30 +14,23 @@ import net.minecraftforge.fml.common.Mod;
 public class ClientEvents {
     @Mod.EventBusSubscriber(modid = SkyForgeMod.MOD_ID, value = Dist.CLIENT)
     public static class ClientForgeEvents {
-
         @SubscribeEvent
         public static void onKeyInput(InputEvent.Key event) {
-            if(net.dilger.sky_forge_mod.util.KeyBinding.OPEN_SKILLSCREEN.consumeClick()) {
+            if(KeyBinding.OPEN_SKILLSCREEN.consumeClick()) {
                 Minecraft.getInstance().player.sendSystemMessage(Component.literal("Pressed a Key!"));
                 //Minecraft.getInstance().player.openMenu(null);
             }
-            if(net.dilger.sky_forge_mod.util.KeyBinding.OPEN_TESTSCREEN.consumeClick()){
+            if(KeyBinding.OPEN_TESTSCREEN.consumeClick()){
                 ClientHooks.openExampleScreen();
             }
         }
-
     }
 
     @Mod.EventBusSubscriber(modid = SkyForgeMod.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static class ClientModBusEvents{
+    public static class ClientModBusEvents {
         @SubscribeEvent
         public static void onKeyRegister(RegisterKeyMappingsEvent event) {
             event.register(KeyBinding.OPEN_SKILLSCREEN);
-        }
-
-        @SubscribeEvent
-        public static void registerGuiOverlays(RegisterGuiOverlaysEvent event) {
-//            event.registerAboveAll("example", ExampleHudOverlay.HUD);
         }
     }
 }

e8338f4bb280471a6c7420bb17a51e8234fa7de2
ReallyMadMax <ettrevivre@gmail.com>
2023-11-21 08:33:46 -0500

    got rif of switch statement and now levels up the proper skill

--- src/main/java/net/dilger/sky_forge_mod/networking/packets/affectPlayerData/SendXPpacket.java
+++ src/main/java/net/dilger/sky_forge_mod/networking/packets/affectPlayerData/SendXPpacket.java
@@ -17,18 +17,17 @@ public class SendXPpacket {
     //this is what is getting encoded / decoded
     //add all xp variables here??
     private static final String MESSAGE_GAIN_XP = "message.sky_forge_mod.gain_xp";
-    private static PlayerSkillXp.SKILL_TYPE skill_type;
+    private PlayerSkillXp.SKILL_TYPE skill_type;
 
-    private final String chosenSkill;
     private final byte amount;
 
     //add variables that you want to be passed to the constructor below
-    public SendXPpacket(String skill, byte amount) {
-        this.chosenSkill = skill;
+    public SendXPpacket(PlayerSkillXp.SKILL_TYPE skill, byte amount){
+        this.skill_type = skill;
         this.amount = amount;
     }
     public SendXPpacket(FriendlyByteBuf buffer) {
-        this(buffer.readUtf(), buffer.readByte());
+        this(PlayerSkillXp.SKILL_TYPE.valueOf(buffer.readUtf()), buffer.readByte());
     }
 
     public void toBytes(FriendlyByteBuf buf) {
@@ -37,7 +36,7 @@ public class SendXPpacket {
 
     //you need one buffer.write per variable you are passing to the constructor
     public void encode(FriendlyByteBuf buffer) {
-        buffer.writeUtf(this.chosenSkill);
+        buffer.writeUtf(String.valueOf(this.skill_type));
         buffer.writeByte(this.amount);
 
     }
@@ -53,30 +52,13 @@ public class SendXPpacket {
             // Notify the player that they have gained a perk
             assert player != null;
 
-            switch(chosenSkill){
-                case "Defense":
-                     skill_type = PlayerSkillXp.SKILL_TYPE.DEFENSE;
-                case "Farming":
-                    skill_type = PlayerSkillXp.SKILL_TYPE.FARMING;
-                case "Fishing":
-                    skill_type = PlayerSkillXp.SKILL_TYPE.FISHING;
-                case "Mining":
-                    skill_type = PlayerSkillXp.SKILL_TYPE.MINING;
-                case "Mobility":
-                    skill_type = PlayerSkillXp.SKILL_TYPE.MOBILITY;
-                case "Offense":
-                    skill_type = PlayerSkillXp.SKILL_TYPE.OFFENSE;
-                case "Trading":
-                    skill_type = PlayerSkillXp.SKILL_TYPE.TRADING;
-            }
-
               // Change the player skill xp
 
             player.getCapability(PlayerSkillXpProvider.PLAYER_SKILL_XP).ifPresent(skill_xp -> {
             skill_xp.addSkillXp(amount, skill_type);
 
-            player.sendSystemMessage(Component.literal("Current Xp " + skill_xp.getSkillXp(skill_type))
-                    .withStyle(ChatFormatting.GOLD));
+            player.sendSystemMessage(Component.literal("Current Xp " + skill_xp.getSkillXp(skill_type)).withStyle(ChatFormatting.GOLD));
+            player.sendSystemMessage(Component.literal(String.valueOf((skill_type))).withStyle(ChatFormatting.BLACK));
             PacketHandling.sentToPlayer(new SkillXpDataSyncS2CPacket(skill_xp.getSkillsXpMap()), player);
             });
 
